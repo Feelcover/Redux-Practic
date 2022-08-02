afd200fe02afe0ddfa37094ce21a392bf667c88f
Andrey Chebanov <pioneerbeat@yandex.ru>
2022-08-02 16:42:03 +0300

    add/new logic and refactoring

--- src/App.js
+++ src/App.js
@@ -1,27 +1,73 @@
 import React from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
+import { addCustomerAction, removeCustomerAction } from "./store/customerReducer";
 
 function App() {
   const dispatch = useDispatch();
-  const cash = useSelector(state => state.cash);
+  const cash = useSelector((state) => state.cashReducer.cash);
+  const customers = useSelector((state) => state.customerReducer.customers);
 
   const addCash = (cash) => {
-      dispatch({type:'ADD_CASH', payload: cash})
-  }
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
 
   const getCash = (cash) => {
-    dispatch({type:'GET_CASH', payload: cash})
-    
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    };
+    dispatch(addCustomerAction(customer));
+  };
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
   }
+
 
   return (
     <div className="App">
       <div className="counter">{cash}</div>
       <div className="container">
-        <button className="button" onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
-        <button className="button" onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+        <button className="button" onClick={() => addCash(Number(prompt()))}>
+          Пополнить счет
+        </button>
+        <button className="button" onClick={() => getCash(Number(prompt()))}>
+          Снять со счета
+        </button>
+        <button className="button" onClick={() => addCustomer(prompt())}>
+          Добавить клиента
+        </button>
       </div>
+      {customers.length > 0 ? (
+        <div>
+          {customers.map((customer) => (
+            <div
+            key= {Date.now()}
+              onClick={() => removeCustomer(customer)}
+              style={{
+                fontSize: "2rem",
+                border: "1px solid black",
+                padding: "10px",
+                marginTop: 5,
+                textAlign: "center",
+              }}
+            >
+              {customer.name}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div
+          style={{ fontSize: "2rem", marginTop: "20px", textAlign: "center" }}
+        >
+          Клиенты отсутствуют
+        </div>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,27 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { legacy_createStore as createStore } from 'redux';
 import {Provider} from "react-redux"
-
-const defaultState = {
-  cash: 5,
-}
+import {store} from "./store/index"
 
 
-const reducer = (state = defaultState, action) => {
-  switch(action.type) {
-      case "ADD_CASH":
-          return {...state, cash: state.cash + action.payload}
-      case "GET_CASH":
-          return {...state, cash: state.cash - action.payload}
 
-      default:
-            return state
-  }
-}
-
-const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
